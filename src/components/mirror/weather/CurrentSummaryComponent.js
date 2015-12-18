9a8137ffa057d7802737ba08e5ab2556f6d657c4
Ben Cates <ben.cates@gmail.com>
2015-12-18 12:46:53 -0700

    Add condition icons

--- src/components/mirror/weather/CurrentSummaryComponent.js
+++ src/components/mirror/weather/CurrentSummaryComponent.js
@@ -5,8 +5,6 @@ import React from 'react';
 require('styles/mirror/weather/CurrentSummary.sass');
 
 const conditionsIcon = icon => {
-  // for "Forecast Font"
-
   switch (icon) {
     case 'clear-day':
       return <span><i className="icon-sun" /></span>;
@@ -14,33 +12,36 @@ const conditionsIcon = icon => {
       return <span><i className="icon-moon" /></span>;
     case 'rain':
       return (<span>
-        <i className="basecloud" />
+        <i className="icon-basecloud" />
         <i className="icon-rain" />
       </span>);
     case 'snow':
       return (<span>
-        <i className="basecloud" />
+        <i className="icon-basecloud" />
         <i className="icon-snow" />
       </span>);
     case 'sleet':
       return (<span>
-        <i className="basecloud" />
+        <i className="icon-basecloud" />
         <i className="icon-sleet" />
       </span>);
     case 'wind':
-      return <span><i className="icon-wind" /></span>;
+      return (<span>
+        <i className="icon-basecloud" />
+        <i className="icon-wind" />
+      </span>);
     case 'fog':
       return <span><i className="icon-mist" /></span>;
     case 'cloudy':
-      return <span><i className="basecloud" /></span>;
+      return <span><i className="icon-cloud" /></span>;
     case 'partly-cloudy-day':
       return (<span>
-        <i className="basecloud" />
+        <i className="icon-cloud" />
         <i className="icon-day" />
       </span>);
     case 'partly-cloudy-night':
       return (<span>
-        <i className="basecloud" />
+        <i className="icon-cloud" />
         <i className="icon-night" />
       </span>);
     default:
